cd69d23b8198eb5c78d0f188e6c5874874afb3a4
Sydney Barlow <sydneybarlow44@gmail.com>
2018-06-02 03:30:11 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  counter = nil
+  symbol = name
+  
   name_hash.each do |name, num|
       
+      if num < counter 
+        num =+ 1 
      
 end
 
