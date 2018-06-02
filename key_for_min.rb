d5f233a51bcc91ddbb6fccab5f627b10862bc570
Sydney Barlow <sydneybarlow44@gmail.com>
2018-06-02 03:47:00 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   
   name_hash.each do |name, num|
       
-      if num < lowest_value || lowest_value == nil
+      if  lowest_value == nil || num < lowest_value
         symbol = name
         lowest_value = num
       end  
