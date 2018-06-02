e879bab41cc70fb63c009089a4fcb3402a503335
Sydney Barlow <sydneybarlow44@gmail.com>
2018-06-02 03:48:11 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -14,28 +14,3 @@ def key_for_min_value(name_hash)
      end
   symbol 
 end
-
-# def LowestSecondLowest(arr)
-#   lowest,second_lowest = nil,nil
-
-#   arr.each do |n|
-
-#     if second_lowest.nil?
-#       second_lowest=n
-#     elsif lowest.nil?
-#       if n>second_lowest
-#         lowest=second_lowest
-#         second_lowest=n
-#       elsif n<second_lowest
-#         lowest=n
-#       end
-#     elsif n<lowest
-#       second_lowest=lowest
-#       lowest=n
-#     elsif n>lowest && n<second_lowest
-#       second_lowest=n
-#     end       
-#   end
-
-#   "#{lowest} #{second_lowest}"
-# end
